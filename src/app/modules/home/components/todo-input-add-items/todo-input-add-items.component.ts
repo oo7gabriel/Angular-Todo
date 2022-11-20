c3a4c27cb92e474fdc9554fa6c0e0ed8dfba11d1
oo7gabriel <gabrielfsacademico@gmail.com>
2022-11-19 23:58:09 -0300

    Adicionando Aplicação To-Do

---
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-items',
+  templateUrl: './todo-input-add-items.component.html',
+  styleUrls: ['./todo-input-add-items.component.scss']
+})
+export class TodoInputAddItemsComponent implements OnInit {
+
+  @Output() public emitItemTaskList = new EventEmitter;
+
+  public addItemTaskList: string = "";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public submitItemTaskList(){
+    console.log(this.addItemTaskList);
+
+
+    this.addItemTaskList = this.addItemTaskList.trim();
+    if(this.addItemTaskList){
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList="";
+    }
+  }
+
+}
